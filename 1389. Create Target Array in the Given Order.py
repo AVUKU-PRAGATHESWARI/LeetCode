e0dc43e70b69586d71c28868deb597e096a3d7c4
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-08-23 11:12:14 +0530

    Moved

--- 1389. Create Target Array in the Given Order.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
-        target = [0]*len(nums)
-        for i in range(len(nums)):
-            value = nums[i]
-            index_tar = index[i]
-            target = target[:index_tar]+[value]+target[index_tar:]
-            target.pop()
-        return target
--- 2798. Number of Employees Who Met the Target .py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
-        count = sum(h>=target for h in hours)
-        return count
-    
