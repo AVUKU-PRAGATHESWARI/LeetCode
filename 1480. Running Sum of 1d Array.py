4e32576f1a2d2015b0a440f02fbc834359062d01
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-08-23 13:58:03 +0530

    July started

--- 1480. Running Sum of 1d Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        sum=0
-        summing=[]
-        for i in nums:
-            sum+=i
-            summing.append(sum)
-        return summing
--- 2367. Number of Arithmetic Triplets.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        result = 0
-        for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                for k in range(j+1,len(nums)):
-                    if nums[j]-nums[i] == diff and nums[k]-nums[j]==diff:
-                        result += 1
-        return result
