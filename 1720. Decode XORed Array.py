b43862fae72f8eb1683669fba34982e2bc482741
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-08-23 11:48:36 +0530

    Day-25

---
+++ 1720. Decode XORed Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        arr = [first]
+        for a in encoded:
+            arr.append(arr[-1] ^ a)
+        return arr
