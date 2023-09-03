de37abb972cc5e24229acf30533ce6bbcc526d7e
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-09-03 19:41:32 +0530

    DAY-29

---
+++ 2194. Cells in a Range on an Excel Sheet.py
@@ -0,0 +1,4 @@
+class Solution:
+    def cellsInRange(self, s: str) -> List[str]:
+        result = []
+        return [(chr(c)+chr(r)) for c in range(ord(s[0]), ord(s[3])+1) for r in range(ord(s[1]), ord(s[4])+1)]
