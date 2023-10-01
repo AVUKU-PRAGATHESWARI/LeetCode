e83fb18cf9abec96879e141b818ec52fa9975337
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-10-01 09:47:04 +0530

    DAY - 1

--- 1636. Sort Array by Increasing Frequency.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def frequencySort(self, nums: List[int]) -> List[int]:
-        r = Counter(nums)
-        return sorted(nums, key=lambda x: (r[x], -x))
--- 2441. Largest Positive Integer That Exists With Its Negative.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findMaxK(self, nums: List[int]) -> int:
-        nums.sort(reverse=True)
-        for i in nums:
-            if -i in nums:
-                return i
-        return -1
-        
