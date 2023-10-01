661a738a0a41bfd5bf6352091e33078c109d9cac
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-10-01 09:45:36 +0530

    Before

--- 2678. Number of Senior Citizens.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def countSeniors(self, details: List[str]) -> int:
-        result = 0
-        for i in details:
-            if int(i[11:13])>60:
-                result+=1 
-        return result 
---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        for i in range(n):
+            if (i+1)%3 == 0:
+                if (i+1)%5 == 0:
+                    answer.append("FizzBuzz")
+                else:
+                    answer.append("Fizz")
+            elif (i+1)%5 == 0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i+1))
+        return answer
+            
