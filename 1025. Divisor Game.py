0ff3cc3468488192b5f295e79e7dd9587ec321e9
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-10-17 18:12:24 +0530

    Greedy

--- 1025. Divisor Game.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def divisorGame(self, n: int) -> bool:
-        return not n%2
--- 1137. N-th Tribonacci Number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def tribonacci(self, n: int) -> int:
-        T = [0,1,1]
-        for i in range(3,n+1):
-            s = T[i-1] + T[i-2] + T[i-3]
-            T.append(s)
-        r = T[n]
-        return r
