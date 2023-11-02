316766dfd86f95dbd12e12c93b0515606bd5c47f
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-11-03 00:12:02 +0530

    Started Trre

--- 190. Reverse Bits.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def reverseBits(self, n: int) -> int:
-        binary = bin(n)[2:]
-        length = len(binary)
-        binary = '0'*(32-length) + binary
-        binary = binary[::-1]
-        n = int(binary,2)
-        return n
-        
--- 69. Sqrt(x).py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        res = x**(0.5)
-        return int(res)        
