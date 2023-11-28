10812d521f6230ff2354603fa3318842df5905b5
AVUKU-PRAGATHESWARI <pragatheswaria@gmail.com>
2023-11-28 15:34:16 +0530

    arrays

---
+++ 378. Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        new = []
+        for i in matrix:
+            new.extend(i)
+        new.sort()
+        return new[k-1]
